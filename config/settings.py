9acecda389d90687b13a8e255e8f1d811c88d4b2
bigge <badboywoo01@gmail.com>
2026-05-19 18:19:21 +0100

    Fix: DB_PASSWORD default + collectstatic before migrate

--- config/settings.py
+++ config/settings.py
@@ -84,7 +84,7 @@ else:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': config('DB_NAME', default='venue_db'),
             'USER': config('DB_USER', default='postgres'),
-            'PASSWORD': config('DB_PASSWORD'),
+            'PASSWORD': config('DB_PASSWORD', default=''),
             'HOST': config('DB_HOST', default='localhost'),
             'PORT': config('DB_PORT', default='5432'),
         }
